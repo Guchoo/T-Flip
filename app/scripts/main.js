e3faae114f53bd495f49b186e9a95dec14f895d2
Torry Tufteland <torry.tufteland@gmail.com>
2015-10-19 13:18:27 +0200

    Assignment on main page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
